1f38b51fce46300467a5006924fe87b1a52b2462
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2024-07-02 09:58:35 +0000

    Cloning prophecy project main branch

---
+++ test/Automation_scala_CodeGen_Quality_RealRepo_scala/pipelines/Scala_E2E_Existing_Pipeline_All_1/code/src/main/scala/io/prophecy/pipelines/scala_e2e_existing_pipeline_all_1/graph/SchemaTransform_1.scala
@@ -0,0 +1,22 @@
+package io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.graph
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.udfs.PipelineInitCode._
+import io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.udfs.UDFs._
+import io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.config.Context
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object SchemaTransform_1 {
+
+  def apply(context: Context, in: DataFrame): DataFrame =
+    in.withColumn("c_new1", concat(col("first_name"), col("last_name")))
+      .drop("account_flags")
+      .withColumnRenamed("phone", "phone_new")
+      .withColumn("c_new2",       concat(col("first_name"), col("last_name")))
+
+}
