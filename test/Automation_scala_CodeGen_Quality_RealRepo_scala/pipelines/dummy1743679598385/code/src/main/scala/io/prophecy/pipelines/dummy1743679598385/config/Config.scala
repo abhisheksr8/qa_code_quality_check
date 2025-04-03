7df42a813db1facca01b0b9d9dc500b23c9b9df9
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-04-03 11:27:43 +0000

    Simple workflow Commit

---
+++ test/Automation_scala_CodeGen_Quality_RealRepo_scala/pipelines/dummy1743679598385/code/src/main/scala/io/prophecy/pipelines/dummy1743679598385/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.dummy1743679598385.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ test/Automation_scala_CodeGen_Quality_RealRepo_scala/pipelines/dummy1743679598385/code/src/main/scala/io/prophecy/pipelines/dummy1743679598385/main.scala
@@ -0,0 +1,36 @@
+package io.prophecy.pipelines.dummy1743679598385
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.dummy1743679598385.config._
+import io.prophecy.pipelines.dummy1743679598385.functions.UDFs._
+import io.prophecy.pipelines.dummy1743679598385.functions.PipelineInitCode._
+import io.prophecy.pipelines.dummy1743679598385.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("dummy1743679598385")
+      .enableHiveSupport()
+      .getOrCreate()
+    val context = Context(spark, config)
+    spark.conf
+      .set("prophecy.metadata.pipeline.uri",                "pipelines/dummy1743679598385")
+    spark.conf.set("spark.default.parallelism",             "4")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    registerUDFs(spark)
+    MetricsCollector.instrument(spark, "pipelines/dummy1743679598385") {
+      apply(context)
+    }
+  }
+
+}
