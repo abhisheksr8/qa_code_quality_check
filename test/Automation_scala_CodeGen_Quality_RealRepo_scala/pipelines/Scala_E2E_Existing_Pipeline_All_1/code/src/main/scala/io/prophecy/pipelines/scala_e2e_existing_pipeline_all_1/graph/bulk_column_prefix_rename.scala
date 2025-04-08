1ca5a8c6e641de39c78f80ce4b2dd83e6d12805d
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-04-08 01:16:43 +0000

    Cloning prophecy project main branch

---
+++ test/Automation_scala_CodeGen_Quality_RealRepo_scala/pipelines/Scala_E2E_Existing_Pipeline_All_1/code/src/main/scala/io/prophecy/pipelines/scala_e2e_existing_pipeline_all_1/graph/bulk_column_prefix_rename.scala
@@ -0,0 +1,30 @@
+package io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.graph
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.udfs.PipelineInitCode._
+import io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.udfs.UDFs._
+import io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.config.Context
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object bulk_column_prefix_rename {
+
+  def apply(context: Context, in: DataFrame): DataFrame = {
+    import org.apache.spark.sql.ProphecyDataFrame
+    ProphecyDataFrame
+      .extendedDataFrame(in)
+      .evaluate_expression("concat('pre_',column_name)",
+                           List("C_CUSTOMER_SK",
+                                "C_CUSTOMER_ID",
+                                "C_CURRENT_CDEMO_SK",
+                                "C_CURRENT_HDEMO_SK"
+                           ),
+                           context.spark
+      )
+  }
+
+}
