660cf62a20e07842d4da93f3ed3e901fa6411ce8
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-05-31 21:25:51 +0000

    Simple workflow Commit

---
+++ test/Automation_scala_CodeGen_Quality_RealRepo_scala/pipelines/dummy1748726692032/code/src/main/scala/io/prophecy/pipelines/dummy1748726692032/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.dummy1748726692032.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ test/Automation_scala_CodeGen_Quality_RealRepo_scala/pipelines/dummy1748726692032/code/src/main/scala/io/prophecy/pipelines/dummy1748726692032/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.dummy1748726692032.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
