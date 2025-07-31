5b4f1ad12aa0967faf98ee695ba665631bf895e2
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-07-31 12:20:33 +0000

    Cloning prophecy project main branch

---
+++ test/Automation_scala_CodeGen_Quality_RealRepo_scala/pipelines/Scala_E2E_Existing_Pipeline_All_1/code/src/test/scala/io/prophecy/pipelines/scala_e2e_existing_pipeline_all_1/graph/Reformat_2Test.scala
@@ -0,0 +1,97 @@
+package io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.graph
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.config._
+import io.prophecy.libs.registerAllUDFs
+import io.prophecy.libs.SparkTestingUtils._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.functions._
+import org.junit.runner.RunWith
+import org.junit.Assert
+import org.scalatest.FunSuite
+import java.time.LocalDateTime
+import org.scalatest.junit.JUnitRunner
+import java.sql.{Date, Timestamp}
+import scala.collection.JavaConverters._
+import java.nio.file.{Files, Paths}
+import java.math.BigDecimal
+
+@RunWith(classOf[JUnitRunner])
+class Reformat_2Test extends FunSuite with DataFrameSuiteBase {
+  import sqlContext.implicits._
+  var context: Context = null
+
+  test("Unit Test ") {
+
+    val dfIn = createDfFromResourceFiles(
+      spark,
+      "/data/io/prophecy/pipelines/scala_e2e_existing_pipeline_all_1/graph/Reformat_2/in/schema.json",
+      "/data/io/prophecy/pipelines/scala_e2e_existing_pipeline_all_1/graph/Reformat_2/in/data/unit_test_.json",
+      "in"
+    )
+    val dfOut = createDfFromResourceFiles(
+      spark,
+      "/data/io/prophecy/pipelines/scala_e2e_existing_pipeline_all_1/graph/Reformat_2/out/schema.json",
+      "/data/io/prophecy/pipelines/scala_e2e_existing_pipeline_all_1/graph/Reformat_2/out/data/unit_test_.json",
+      "out"
+    )
+
+    val dfOutComputed =
+      io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.graph
+        .Reformat_2(context, dfIn)
+    val res = assertDFEquals(
+      dfOut.select("customer_id",
+                   "first_name",
+                   "last_name",
+                   "phone",
+                   "email",
+                   "country_code",
+                   "account_open_date",
+                   "account_flags"
+      ),
+      dfOutComputed.select("customer_id",
+                           "first_name",
+                           "last_name",
+                           "phone",
+                           "email",
+                           "country_code",
+                           "account_open_date",
+                           "account_flags"
+      ),
+      maxUnequalRowsToShow,
+      1.0
+    )
+    val msg = if (res.isLeft) res.left.get.getMessage else ""
+    Assert.assertTrue(msg, res.isRight)
+  }
+
+  override def beforeAll() = {
+    super.beforeAll()
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    registerAllUDFs(spark)
+
+    val fabricName = System.getProperty("fabric", "default")
+    val confFilePath = Paths
+      .get(getClass.getResource(s"/config/${fabricName}.json").toURI)
+      .toString
+
+    val config =
+      ConfigurationFactoryImpl.getConfig(Array("--confFile", confFilePath))
+
+    context = Context(spark, config)
+
+    val dfProphecy_pipelines_scala_e2e_existing_pipeline_all_1_graph_Lookup_1 =
+      createDfFromResourceFiles(
+        spark,
+        "/data/io/prophecy/pipelines/scala_e2e_existing_pipeline_all_1/graph/Lookup_1/schema.json",
+        "/data/io/prophecy/pipelines/scala_e2e_existing_pipeline_all_1/graph/Lookup_1/data.json",
+        port = "in"
+      )
+    io.prophecy.pipelines.scala_e2e_existing_pipeline_all_1.graph.Lookup_1(
+      context,
+      dfProphecy_pipelines_scala_e2e_existing_pipeline_all_1_graph_Lookup_1
+    )
+  }
+
+}
