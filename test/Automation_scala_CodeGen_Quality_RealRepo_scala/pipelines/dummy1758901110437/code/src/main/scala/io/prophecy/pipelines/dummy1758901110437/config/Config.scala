65c492268e29f881c06975f3634458c88de12f0f
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-09-26 15:39:46 +0000

    Simple workflow Commit

---
+++ test/Automation_scala_CodeGen_Quality_RealRepo_scala/pipelines/dummy1758901110437/code/src/main/scala/io/prophecy/pipelines/dummy1758901110437/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.dummy1758901110437.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ test/Automation_scala_CodeGen_Quality_RealRepo_scala/pipelines/dummy1758901110437/code/src/main/scala/io/prophecy/pipelines/dummy1758901110437/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.dummy1758901110437.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
